2fd10d321bc830476edb3cbdd6d19dec9d08dc73
Johanna <joisk026@gmail.com>
2026-05-27 21:26:36 -0500

    🚀 multitenat

--- schemaTypes/negocio.js
+++ schemaTypes/negocio.js
@@ -3,9 +3,14 @@ export default {
   title: 'Negocios Registrados',
   type: 'document',
   fields: [
+    // 🏢 Datos de Identidad Principal
     { name: 'nombre', title: 'Nombre del Negocio', type: 'string' },
     { name: 'slug', title: 'Identificador (Tenant ID)', type: 'slug', options: { source: 'nombre' } },
-    { name: 'logo', title: 'Logo', type: 'image' },
-    { name: 'colorPrincipal', title: 'Color de Marca', type: 'string' },
+    { name: 'nit', title: 'NIT / RUT', type: 'string' },
+    { name: 'direccion', title: 'Dirección Física', type: 'string' },
+    { name: 'telefono', title: 'Teléfono de Contacto', type: 'string' },
+    
+    // 🎨 Identidad Visual (Para que el POS cambie de color según el cliente)
+    { name: 'colordark', title: 'Color de Marca (Ej: #000A6F)', type: 'string', initialValue: '#000A6F' },
   ]
 }
