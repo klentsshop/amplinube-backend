64a5c080a72837150f2a53f93e22b03901403932
Johanna <joisk026@gmail.com>
2026-05-31 07:03:57 -0500

    feat: blindaje de excel

--- schemaTypes/mesero.js
+++ schemaTypes/mesero.js
@@ -12,6 +12,13 @@ export default {
       description: 'Escriba el nombre tal cual quiere que aparezca en el POS',
     },
     {
+      name: 'activo',
+      title: '¿Usuario Activo en el POS?',
+      type: 'boolean',
+      initialValue: true, // Por defecto, cuando lo crean, entra activo
+      description: 'Si se desactiva, perderá el acceso inmediato al sistema visual del POS.'
+    },
+    {
             name: 'tenant', // 👈 ESTE ES EL NUEVO JEFE
             title: 'Negocio',
             type: 'string',
